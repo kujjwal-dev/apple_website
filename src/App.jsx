a5f330161cb7c359ac0aaa879f7651476b158f79
kujjwal-dev <trptrp098@gmail.com>
2024-04-29 18:50:38 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 
  const App = () => {
@@ -12,6 +16,10 @@ import Highlights from './components/Highlights'
     <Navbar />
     < Hero  />
     <Highlights />
+    <Model />
+    <Features/>
+    <HowItWorks />
+    <Footer />
 
    </main>
   )
